6cb42ac714c01b8647657a20fc213d722d5d7e40
fulldev319 <maslea319@gmail.com>
2024-07-13 17:57:36 -0400

    implement Question Page

---
+++ frontend/src/components/QuestionPage.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+const QuestionPage = () => {
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/questions")
+      .then((response) => setQuestions(response.data));
+  }, []);
+
+  const handleAnswerChange = (questionId, optionIndex) => {
+    const newAnswers = [...answers];
+    newAnswers[questionId - 1] = optionIndex;
+    setAnswers(newAnswers);
+  };
+
+  const handleSubmit = () => {
+    axios.post("http://localhost:5000/submit", { answers }).then((response) => {
+      history.push(`/result?type=${response.data.result}`);
+    });
+  };
+
+  return (
+    <div>
+      {questions.map((question) => (
+        <div key={question.id}>
+          <p>{question.question}</p>
+          {question.options.map((option, index) => (
+            <div key={index}>
+              <input
+                type="radio"
+                name={`question-${question.id}`}
+                value={index}
+                onChange={() => handleAnswerChange(question.id, index)}
+              />
+              {option}
+            </div>
+          ))}
+        </div>
+      ))}
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+export default QuestionPage;
