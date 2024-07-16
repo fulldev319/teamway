df3460e690709d2fff34d8e129c3a67b6e555438
fulldev319 <maslea319@gmail.com>
2024-07-16 14:12:31 -0400

    fix router issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import QuestionPage from "./components/QuestionPage";
 import ResultPage from "./components/ResultPage";
 
 const App = () => (
-  <BrowserRouter>
-    <Router>
-      <Routes>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/questions" component={QuestionPage} />
-        <Route path="/result" component={ResultPage} />
-      </Routes>
-    </Router>
-  </BrowserRouter>
+  <Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/questions" element={<QuestionPage />} />
+      <Route path="/result" element={<ResultPage />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
